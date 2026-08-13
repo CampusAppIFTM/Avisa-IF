83b650fb21628e865c9e505e5b0b9ec6e6f930ff
Laila Cristina <laila.alves@estudante.iftm.edu.br>
2026-08-13 17:10:06 -0300

    Parte da Firebase do projeto do Angotti que pelo visto não está funcionando

--- App.js
+++ App.js
@@ -1,37 +1,74 @@
-import { useState } from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { StackEvents } from './Navigators/StackEvents';
-import { createDrawerNavigator } from '@react-navigation/drawer';
-import {
-  useFonts,
-  Poppins_400Regular,
-  Poppins_700Bold,
-} from '@expo-google-fonts/poppins';
+import { StyleSheet, Text, View, Button, ActivityIndicator, Image } from "react-native";
+import { useState } from "react";
+import { GoogleSignin } from "@react-native-google-signin/google-signin";
 
-const Drawer = createDrawerNavigator();
+//funções de autenticação
+export const onLogin = async () => {
+  const user = await GoogleSignin.signIn();
+  return user;
+};
 
-export default function App() {
-  const [eventos, setEventos] = useState([]);
-   const [avisos, setAvisos] = useState([]);
-   const [fontsLoaded] = useFonts({
-    Poppins_400Regular,
-    Poppins_700Bold,
-  });
-  
-  if (!fontsLoaded) return null
+export const onLogout = async () => {
+  return await GoogleSignin.signOut();
+};
+
+GoogleSignin.configure({
+  webClientId: "676797397237-pjipptjgb1nuaomvcm6rd6dpt19jl06n.apps.googleusercontent.com",
+});
+
+// Telas
+const LoginScreen = ({ login }) => {
+  const [isSigninInProgress, setIsSigninInProgress] = useState(false);
+
   return (
-    <NavigationContainer>
-      <Drawer.Navigator>
-        <Drawer.Screen
-          name="Inicio"
-          options={{
-            headerShown: false,
-          }}>
-          {(props) => (
-            <StackEvents {...props} eventos={eventos} setEventos={setEventos} avisos={avisos} setAvisos={setAvisos} />
-          )}
-        </Drawer.Screen>
-      </Drawer.Navigator>
-    </NavigationContainer>
+    <View style={styles.layout}>
+      {isSigninInProgress && <ActivityIndicator />}
+      <Text style={styles.title}>Login</Text>
+      <Button
+        title="entrar"
+        onPress={() => {
+          setIsSigninInProgress(true);
+          onLogin().then((user) => {
+            console.log(user);
+            login(user);
+          });
+        }}
+      />
+    </View>
   );
-}
+};
+
+const HomeScreen = ({ user, login }) => (
+  <View style={styles.layout}>
+    <Text style={styles.title}>Home</Text>
+    <Image
+      style={{ width: 300, height: 300 }}
+      source={{
+        uri: user.user.photo,
+      }}
+    />
+    <Button title="Sair" onPress={() => onLogout().then(() => login(false))} />
+  </View>
+);
+
+const App = () => {
+  const [user, setUser] = useState(false);
+  return <View style={styles.container}>{user ? <HomeScreen user={user} login={setUser} /> : <LoginScreen login={setUser} />}</View>;
+};
+
+export default App;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  layout: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  title: {
+    fontSize: 32,
+    marginBottom: 16,
+  },
+});
